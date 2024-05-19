480b37f886505191b368ee582a19ad344c3785e5
romanost <romanost82@walla.co.il>
2024-05-19 13:53:58 +0300

    Update mobile_osm_flutter.dart

--- lib/src/widgets/mobile_osm_flutter.dart
+++ lib/src/widgets/mobile_osm_flutter.dart
@@ -262,7 +262,7 @@ class PlatformView extends StatelessWidget {
   Widget build(BuildContext context) {
     if (defaultTargetPlatform == TargetPlatform.iOS) {
       return UiKitView(
-        key: mobileKey,
+        // key: mobileKey,
         viewType: 'plugins.dali.hamza/osmview',
         onPlatformViewCreated: onPlatformCreatedView,
         creationParams: getParams(
